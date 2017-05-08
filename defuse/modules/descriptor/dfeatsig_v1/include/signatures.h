f55cd558f01a950a3ebfc3c3ec3e10a4e212e2b2
skletz <skletz@edu.aau.at>
2017-05-08 09:54:00 +0200

    updates project structure for using defuse on windows and linux

--- defuse/modules/descriptor/dfeatsig_v1/include/signatures.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TFSIGNATURES__ALL_H__
-#define __TFSIGNATURES__ALL_H__
-
-#include "../../featsig/include/signatures.h"
-#include "../src/tpct_signatures.hpp"
-#include "../src/constants.h"
-#include "../src/tf_signatures.h"
-
-#endif //__TFSIGNATURES__ALL_H__
