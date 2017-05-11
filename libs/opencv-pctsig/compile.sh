4872aec32c71bf329cc50d7d2d4ce03f49392c2b
Sabrina Kletz (Projektmitarbeiterin) <sabrina@dodeka4.itec.uni-klu.ac.at>
2017-05-11 07:24:40 +0200

    updates makefile

--- libs/opencv-pctsig/compile.sh
+++ libs/opencv-pctsig/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -shared -Wl,-soname,libpctsignatures.so.1.0 -fPIC -std=c++0x -I/usr/local/include -L/usr/local/lib -lopencv_core -lopencv_highgui -lopencv_features2d -opencv_imgproc include/*.h src/*.cpp src/*.hpp -o libpctsignatures.so.1.0
+g++ -shared -Wl,-soname,libpctsignatures.so.1.0 -fPIC -std=c++0x -I/opt/local/include -L/opt/local/lib -lopencv_core -lopencv_highgui -lopencv_features2d -opencv_imgproc cvpctsig/include/*.h cvpctsig/src/*.cpp cvpctsig/src/*.hpp -o libpctsignatures.so.1.0
 
